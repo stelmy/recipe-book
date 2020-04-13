9a4a48e883cae9a35ed440edfa15a6e4bf9c0fd3
Marcin Stelmach <stelmyit@gmail.com>
2020-04-13 15:45:58 +0200

    Directives

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,13 +11,18 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe(
       'Shakshuka',
-      'This is a recipe for shakshuka',
+      'Shakshuka is a classic North African and Middle Eastern dish and one that’s eaten for breakfast or any meal of the day. It’s made from simple, healthy ingredients and is vegetarian. Shakshuka literally means “a mixture” and the traditional version uses tomatoes, onions and spices as the base with eggs poached on top.',
       'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg'
     ),
     new Recipe(
       'Jambalaya',
-      'This is a recipe for jambalaya',
+      'Hands-down the best jambalaya recipe! It is surprisingly easy to make, customizable with your favorite proteins (I used chicken, shrimp and Andouille sausage), and full of bold, zesty, Cajun jambalaya flavors that everyone will love.',
       'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32-1100x1650.jpg'
+    ),
+    new Recipe(
+      'Roast Chicken',
+      'Simple Roast Chicken flavored with garlic, butter and herbs, then oven roasted to a golden, crispy, and juicy perfection! EASY, simple to make and flavorful, this is the BEST roast chicken, and it’s perfect for weekend or weeknight dinners.',
+      'https://diethood.com/wp-content/uploads/2019/07/Whole-Roast-Chicken.jpg'
     )
   ];
 
